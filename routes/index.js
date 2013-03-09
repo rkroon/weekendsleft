46d31263bdb3432978a3c4cfeb7dfa109915ab35
Richard Kroon <richard.kroon@gmail.com>
2013-03-09 17:00:50 +1100

    Look, Feel, Moment.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ exports.index = function(request, response)
 	var info = {};
 	info.weekendsleft = "43";
 	info.timeleftthisweekend = "34 hours, 40 minutes";
-	info.title = "Weekends Left - How are you spending your life?"
+	info.title = "Weekends Left"
 	response.render ('index', info, function (error, html) 
 	{
 		if (error) throw (error);
