6af85b312e74c12915ca130bf219a21135956d13
Richard Kroon <richard.kroon@gmail.com>
2013-03-09 16:02:36 +1100

    Look and feel

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(request, response)
+{
+	var info = {};
+	info.weekendsleft = "43";
+	info.timeleftthisweekend = "34 hours, 40 minutes";
+	info.title = "Weekends Left - How are you spending your life?"
+	response.render ('index', info, function (error, html) 
+	{
+		if (error) throw (error);
+		else
+		{
+			response.write (html);
+			response.end ();
+		}
+	});
 };
